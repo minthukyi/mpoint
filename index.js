de83218e0d3e36216036ec9c25d968f6eb2c942b
minthukyi <minthukyi1312@gmail.com>
2020-03-15 17:13:08 +0630

    test

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ bot.onSubscribe(response => {
 });
 
 
-bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) =>
-    onFinish(new TextMessage(`Mingalarpar ${userProfile.name} Welcome from M-Points! You can order phone bills cards from our shop!`)));
+//bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) =>
+    //onFinish(new TextMessage(`Mingalarpar ${userProfile.name} Welcome from M-Points! You can order phone bills cards from our shop!`)));
 
 bot.on(BotEvents.CONVERSATION_STARTED,(userProfile, isSubscribed, context, onFinish) =>{
     bot.sendMessage(userProfile.userProfile, new TextMessage('Get Started',
