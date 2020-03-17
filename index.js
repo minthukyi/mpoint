99fe097e9d384a8b84981cef68e253f86ea7a38d
minthukyi <61039824+minthukyi@users.noreply.github.com>
2020-03-18 00:24:27 +0630

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFi
                 "ActionType": "reply",
                 "BgColor": "#85bb65",
                 "BgMediaType": "picture",
-                "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3kFg5hREh-RwYKclgeTJ7c8i3w0l_85tlDbdpYLIZZEXBMFe8"
+                "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3kFg5hREh-RwYKclgeTJ7c8i3w0l_85tlDbdpYLIZZEXBMFe8",
                 "Text": "MPT-1000",
                 "Rows": 1,
                 "Columns": 4
