87798ba5b6feb007d0611ee9867d7fadcfdc5b93
minthukyi <61039824+minthukyi@users.noreply.github.com>
2020-04-03 21:28:24 +0630

    Update index.js

--- index.js
+++ index.js
@@ -148,24 +148,37 @@ bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFi
         response.send(new RichMediaMessage(SAMPLE_RICH_MEDIA));
    }
      if(message.text === "Mytel"){
-    // const SAMPLE_KEYBOARD_MESSAGE = {
-         {
-                "Type": "keyboard",
-                "InputFieldState": "hidden",
-                "DefaultHeight": false,
-                "BgColor": '#006600',
-                "Buttons": [
-                    {
-                        "Columns": 6,
-                        "Rows": 1,
-                        "BgColor": '#009900',
-                        "ActionType": "reply",
-                        "ActionBody": "Mytel",
-                        "Text": "<font color='#ffffff'>Mytel-1000</font>"
-             }
-            ]
-         }
-        }
+     const SAMPLE_KEYBOARD = {
+     {
+	"Type": "keyboard",
+	"Buttons": [{
+		"Columns": 3,
+		"Rows": 2,
+		"Text": "<font color=\"#494E67\">Smoking</font><br><br>",
+		"TextSize": "medium",
+		"TextHAlign": "center",
+		"TextVAlign": "bottom",
+		"ActionType": "reply",
+		"ActionBody": "Smoking",
+		"BgColor": "#f7bb3f",
+		"Image": "https: //s12.postimg.org/ti4alty19/smoke.png"
+	}, {
+		"Columns": 3,
+		"Rows": 2,
+		"Text": "<font color=\"#494E67\">Non Smoking</font><br><br>",
+		"TextSize": "medium",
+		"TextHAlign": "center",
+		"TextVAlign": "bottom",
+		"ActionType": "reply",
+		"ActionBody": "Non smoking",
+		"BgColor": "# f6f7f9",
+		"Image": "https: //s14.postimg.org/us7t38az5/Nonsmoke.png"
+	}]
+}
+ };
+       response.send(new KeyboardMessage(SAMPLE_KEYBOARD)); 
+ }
+       
 /*     const SAMPLE_RICH_MEDIA = {
      "ButtonsGroupColumns": 4,
      "ButtonsGroupRows": 1,
